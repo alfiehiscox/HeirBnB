f4beeba5cbf9fec9c596dd533e65cd3ba1563319
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-16 16:25:22 +0000

    Added show capablities, create and links

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -1,14 +1,37 @@
 class CastlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @castles = Castle.all
   end
 
   def new
+    @castle = Castle.new
   end
 
   def create
+    @castle = Castle.new(castles_params)
+    @castle.user = current_user
+    if @castle.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
+    @castle = Castle.find(params[:id])
+  end
+
+  private
+
+  def castles_params
+    params.require(:castle).permit(:name, :address, :price_per_night)
   end
 end
+
+# # Castle
+# - name
+# - address
+# - ppn
+# - user_id
