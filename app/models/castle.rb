244cbd2c1bb136ff7ccc503f7efb40b9050b76e4
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-19 11:34:47 +0000

    Added long/lat fields in Castle Model. Geocoder gem installed. Linking Addresses. Seeds changed.

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -3,4 +3,6 @@ class Castle < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings, dependent: :destroy
   validates :name, :address, :price_per_night, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
